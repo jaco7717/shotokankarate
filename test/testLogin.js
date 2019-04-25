cf05d133bae1beb5237a320cdfa07547f0d9e2d6
JacobVB <jaco7717@gmail.com>
2019-04-25 11:46:09 +0200

    html taps

--- test/testLogin.js
+++ test/testLogin.js
@@ -1,1 +1,19 @@
-//her skal vi lave test af 
+//her skal vi lave test af
+
+const app = require('../karate.js');
+const request = require('supertest');
+const should = require('should');
+
+describe('test  - async', function () {
+
+    it("test af password/username", async function () {
+        const response = await request(app)
+            .get('https://shotokankarate.herokuapp.com/api/login')
+            .expect(200)
+            .expect('Content-Type', /json/);
+        const login = response.body;
+
+        login[0].username.should.be.equal('a');
+        login[0].password.should.be.equal('a');
+    });
+});
