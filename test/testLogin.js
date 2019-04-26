fc6fe9c0f1839700e64e7860be3205f1e5543c9e
Henrik de Laurent Andersen <henrikdelaurent@outlook.dk>
2019-04-26 09:34:19 +0200

    Test af login og news

--- test/testLogin.js
+++ test/testLogin.js
@@ -4,11 +4,10 @@ const app = require('../karate.js');
 const request = require('supertest');
 const should = require('should');
 
-describe('test  - async', function () {
+describe('Login test', function () {
 
-    it("test af password/username", async function () {
+    it("TEST1 - test of correct password/username a/a", async function () {
         const response = await request(app)
-            //.get('https://shotokankarate.herokuapp.com/api/login')
             .get('/api/login')
             .expect(200)
             .expect('Content-Type', /json/);
@@ -16,5 +15,84 @@ describe('test  - async', function () {
 
         login[0].username.should.be.equal('a');
         login[0].password.should.be.equal('a');
+    });
+
+    it("TEST2 - test of incorrect password/username a/a", async function () {
+        const response = await request(app)
+            .get('/api/login')
+            .expect(200)
+            .expect('Content-Type', /json/);
+        const login = response.body;
+
+        login[0].username.should.not.be.equal('c');
+        login[0].password.should.not.be.equal('y');
+    });
+    it("TEST3 - test of correct password/username b/b", async function () {
+        const response = await request(app)
+            .get('/api/login')
+            .expect(200)
+            .expect('Content-Type', /json/);
+        const login = response.body;
+
+        login[1].username.should.be.equal('b');
+        login[1].password.should.be.equal('b');
+    });
+
+    it("TEST4 - test of incorrect password/username b/b", async function () {
+        const response = await request(app)
+            .get('/api/login')
+            .expect(200)
+            .expect('Content-Type', /json/);
+        const login = response.body;
+
+        login[0].username.should.not.be.equal('9');
+        login[0].password.should.not.be.equal('#1');
+    });
+});
+
+describe('News test', function () {
+
+    it("TEST5 - test of correct headline/content test1", async function () {
+        const response = await request(app)
+            .get('/api/news')
+            .expect(200)
+            .expect('Content-Type', /json/);
+        const news = response.body;
+
+        news[0].headline.should.be.equal('test1');
+        news[0].content.should.be.equal('test1');
+    });
+
+    it("TEST6 - test of incorrect headline/content test1", async function () {
+        const response = await request(app)
+            .get('/api/news')
+            .expect(200)
+            .expect('Content-Type', /json/);
+        const news = response.body;
+
+        news[0].headline.should.not.be.equal('test');
+        news[0].content.should.not.be.equal('12345');
+    });
+
+    it("TEST7 - test of correct headline/content test2", async function () {
+        const response = await request(app)
+            .get('/api/news')
+            .expect(200)
+            .expect('Content-Type', /json/);
+        const news = response.body;
+
+        news[1].headline.should.be.equal('test2');
+        news[1].content.should.be.equal('test2');
+    });
+
+    it("TEST8 - test of incorrect headline/content test2", async function () {
+        const response = await request(app)
+            .get('/api/news')
+            .expect(200)
+            .expect('Content-Type', /json/);
+        const news = response.body;
+
+        news[1].headline.should.not.be.equal('test');
+        news[1].content.should.not.be.equal('12345');
     });
 });
