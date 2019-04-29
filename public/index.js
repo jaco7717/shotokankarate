3b75356a6464f265d7e15423cf428c0725428f4d
emillouvmand <emillouvmand@gmail.com>
2019-04-29 10:00:02 +0200

    Console log til newsUserPage funktion

--- public/index.js
+++ public/index.js
@@ -27,6 +27,7 @@ async function getNews() {
 async function getNewsUserPage() {
     const [template, userResponse] =
         await Promise.all([fetch('/newsUserPage.hbs'), fetch('https://shotokankarate.herokuapp.com/api/news')]);
+    console.log("kører funktionen getNewsUserPage?");
     const templateText = await template.text();
     const newsUserPage = await userResponse.json();
     const compiledTemplate = Handlebars.compile(templateText);
