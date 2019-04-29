f09139cd146fb40854ada8fe139dc19ab7636dcf
emillouvmand <emillouvmand@gmail.com>
2019-04-29 10:15:57 +0200

    content js tilføjet

---
+++ public/content.js
@@ -0,0 +1,23 @@
+onload = async () => {
+    update();
+    addNews();
+};
+
+async function update() {
+    const overskrift = document.querySelector('#headline');
+    const tekst = document.querySelector('#content');
+
+    overskrift.value = '';
+    tekst.value = '';
+    getNewsUserPage()
+}
+
+async function getNewsUserPage() {
+    const [template, userResponse] =
+        await Promise.all([fetch('/newsUserPage.hbs'), fetch('https://shotokankarate.herokuapp.com/api/news')]);
+    console.log("kører funktionen getNewsUserPage?");
+    const templateText = await template.text();
+    const newsUserPage = await userResponse.json();
+    const compiledTemplate = Handlebars.compile(templateText);
+    document.querySelector('#nyhederBrugerside').innerHTML = compiledTemplate({newsUserPage});
+}
--- public/index.js
+++ public/index.js
@@ -12,7 +12,6 @@ async function update() {
     overskrift.value = '';
     tekst.value = '';
     getNews();
-    getNewsUserPage()
 }
 
 async function getNews() {
@@ -24,15 +23,7 @@ async function getNews() {
     document.querySelector('#nyheder').innerHTML = compiledTemplate({news});
 }
 
-async function getNewsUserPage() {
-    const [template, userResponse] =
-        await Promise.all([fetch('/newsUserPage.hbs'), fetch('https://shotokankarate.herokuapp.com/api/news')]);
-    console.log("kører funktionen getNewsUserPage?");
-    const templateText = await template.text();
-    const newsUserPage = await userResponse.json();
-    const compiledTemplate = Handlebars.compile(templateText);
-    document.querySelector('#nyhederBrugerside').innerHTML = compiledTemplate({newsUserPage});
-}
+
 
 async function addNews() {
     document.querySelector('#saveNews').onclick = () => {
