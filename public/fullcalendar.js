d545a2fa7dd5cbb40be995f73db256556e6795c9
JacobVB <jaco7717@gmail.com>
2019-05-04 20:38:18 +0200

    forsøg med ikke at kunne flytte events

--- public/fullcalendar.js
+++ public/fullcalendar.js
@@ -4634,20 +4634,7 @@ function View(element, calendar, viewName) {
 	---------------------------------------------------------------------------------*/
 	
 	
-	function moveEvents(events, dayDelta, minuteDelta, allDay) {
-		minuteDelta = minuteDelta || 0;
-		for (var e, len=events.length, i=0; i<len; i++) {
-			e = events[i];
-			if (allDay !== undefined) {
-				e.allDay = allDay;
-			}
-			addMinutes(addDays(e.start, dayDelta, true), minuteDelta);
-			if (e.end) {
-				e.end = addMinutes(addDays(e.end, dayDelta, true), minuteDelta);
-			}
-			normalizeEvent(e, options);
-		}
-	}
+	
 	
 	
 	function elongateEvents(events, dayDelta, minuteDelta) {
