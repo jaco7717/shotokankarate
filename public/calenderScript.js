c5c717ceb6dd2019f19f70775a3d7947f4ec2507
JacobVB <jaco7717@gmail.com>
2019-05-06 12:22:39 +0200

    start consolelog

--- public/calenderScript.js
+++ public/calenderScript.js
@@ -74,6 +74,9 @@ $(document).ready(function () {
                 let url = 'https://shotokankarate.herokuapp.com/api/calender';
                 let data = {title: title, date: new Date(start.year, start.month, start.day, hourInt, minInt), content: content};
 console.log(date);
+console.log(start);
+                console.log(end);
+                console.log(allDay);
                 fetch(url, {
                     method: "POST",
                     body: JSON.stringify(data),
