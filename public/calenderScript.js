df43ad1e807a74cd00577f2cbb4ef6b8c68f7eaf
JacobVB <jaco7717@gmail.com>
2019-05-06 12:58:20 +0200

    todate

--- public/calenderScript.js
+++ public/calenderScript.js
@@ -70,12 +70,15 @@ $(document).ready(function () {
 
             if (title && content) {
 
+                let dato = start.toDate();
+
 
                 let url = 'https://shotokankarate.herokuapp.com/api/calender';
-                let data = {title: title, date: new Date(start.getFullYear(),start.getMonth(),start.getDay(), hourInt, minInt), content: content};
-                console.log(end.getDay());
-console.log(start.toDate())
+                let data = {title: title, date: new Date(dato.getFullYear(),dato.getMonth(),dato.getDay(), hourInt, minInt), content: content};
 
+console.log(dato.getFullYear());
+                console.log(dato.getMonth());
+                console.log(dato.getDay());
                 fetch(url, {
                     method: "POST",
                     body: JSON.stringify(data),
