7170e33e22548ac1494904379f3edd3cb0829830
JacobVB <jaco7717@gmail.com>
2019-05-06 08:59:22 +0200

    fjern af drop

--- public/calenderScript.js
+++ public/calenderScript.js
@@ -1,174 +1,149 @@
+$(document).ready(function () {
+    let date = new Date();
+    let d = date.getDate();
+    let m = date.getMonth();
+    let y = date.getFullYear();
 
-    $(document).ready(function () {
-        let date = new Date();
-        let d = date.getDate();
-        let m = date.getMonth();
-        let y = date.getFullYear();
+    /*  className colors
 
-        /*  className colors
+    className: default(transparent), important(red), chill(pink), success(green), info(blue)
 
-        className: default(transparent), important(red), chill(pink), success(green), info(blue)
-
-        */
+    */
 
 
-        /* initialize the external events
-        -----------------------------------------------------------------*/
+    /* initialize the external events
+    -----------------------------------------------------------------*/
 
-        $('#external-events div.external-event').each(function () {
+    $('#external-events div.external-event').each(function () {
 
-            // create an Event Object (http://arshaw.com/fullcalendar/docs/event_data/Event_Object/)
-            // it doesn't need to have a start or end
-            let eventObject = {
-                title: $.trim($(this).text()) // use the element's text as the event title
-            };
+        // create an Event Object (http://arshaw.com/fullcalendar/docs/event_data/Event_Object/)
+        // it doesn't need to have a start or end
+        let eventObject = {
+            title: $.trim($(this).text()) // use the element's text as the event title
+        };
 
-            // store the Event Object in the DOM element so we can get to it later
-            $(this).data('eventObject', eventObject);
+        // store the Event Object in the DOM element so we can get to it later
+        $(this).data('eventObject', eventObject);
 
-            // make the event draggable using jQuery UI
-            $(this).draggable({
-                zIndex: 999,
-                revert: true,      // will cause the event to go back to its
-                revertDuration: 0  //  original position after the drag
-            });
-
+        // make the event draggable using jQuery UI
+        $(this).draggable({
+            zIndex: 999,
+            revert: true,      // will cause the event to go back to its
+            revertDuration: 0  //  original position after the drag
         });
-
-
-        /* initialize the calendar
-        -----------------------------------------------------------------*/
-
-        let calendar = $('#calendar').fullCalendar({
-            header: {
-                left: 'title',
-                center: 'agendaDay,agendaWeek,month',
-                right: 'prev,next today'
-            },
-            editable: true,
-            firstDay: 1, //  1(Monday) this can be changed to 0(Sunday) for the USA system
-            selectable: true,
-            defaultView: 'month',
-
-            axisFormat: 'h:mm',
-            columnFormat: {
-                month: 'ddd',    // Mon
-                week: 'ddd d', // Mon 7
-                day: 'dddd M/d',  // Monday 9/7
-                agendaDay: 'dddd d'
-            },
-            titleFormat: {
-                month: 'MMMM yyyy', // September 2009
-                week: "MMMM yyyy", // September 2009
-                day: 'MMMM yyyy'                  // Tuesday, Sep 8, 2009
-            },
-            allDaySlot: false,
-            selectHelper: true,
-
-            select: function (start, end, allDay) {
-                let headline = prompt('headline Title:');
-                let content = prompt('content:');
-                if (headline && content) {
-
-
-                    let url = 'https://shotokankarate.herokuapp.com/api/calender';
-                    let data = {headline: headline, date: new Date(y, m, d ), content: content};
-
-                    fetch(url, {
-                        method: "POST",
-                        body: JSON.stringify(data),
-                        headers: { 'Content-Type': 'application/json'}
-                    })
-                        .then(resultat => {
-                            if (resultat.status >= 400)
-                                throw new Error(resultat.status);
-                            else
-                                return resultat.json();
-                        })
-                        .then(resultat => console.log(`Resultat: %o`, resultat))
-                        .catch(fejl => console.log('Fejl: ' + fejl));
-
-
-                    calendar.fullCalendar('renderEvent',
-                        {
-                            title: title,
-                            start: start,
-                            end: end,
-                            allDay: allDay
-                        },
-                        true // make the event "stick"
-                    );
-                }
-                calendar.fullCalendar('unselect');
-            },
-            droppable: true, // this allows things to be dropped onto the calendar !!!
-            drop: function (date, allDay) { // this function is called when something is dropped
-
-                // retrieve the dropped element's stored Event Object
-                let originalEventObject = $(this).data('eventObject');
-
-                // we need to copy it, so that multiple events don't have a reference to the same object
-                let copiedEventObject = $.extend({}, originalEventObject);
-
-                // assign it the date that was reported
-                copiedEventObject.start = date;
-                copiedEventObject.allDay = allDay;
-
-                // render the event on the calendar
-                // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
-                $('#calendar').fullCalendar('renderEvent', copiedEventObject, true);
-
-                // is the "remove after drop" checkbox checked?
-                if ($('#drop-remove').is(':checked')) {
-                    // if so, remove the element from the "Draggable Events" list
-                    $(this).remove();
-                }
-
-            },
-
-            events: [
-                {
-                    headline: 'Special event i Herning ',
-                    content: 'Hele dagen er der baks og riv',
-                    date: new Date(y, m, 1),
-
-                },
-                {
-                    id: 999,
-                    title: 'Repeating Event',
-                    start: new Date(y, m, d - 3, 16, 0),
-                    allDay: false,
-                    className: 'info'
-                },
-                {
-                    id: 999,
-                    title: 'Repeating Event',
-                    start: new Date(y, m, d + 4, 16, 0),
-                    allDay: false,
-                    className: 'info'
-                },
-                {
-                    title: 'Meeting',
-                    start: new Date(y, m, d, 10, 30),
-                    allDay: false,
-                    className: 'important'
-                },
-                {
-                    title: 'Lunch',
-                    start: new Date(y, m, d, 12, 0),
-                    end: new Date(y, m, d, 14, 0),
-                    allDay: false,
-                    className: 'important'
-                },
-                {
-                    title: 'Birthday Party',
-                    start: new Date(y, m, d + 1, 19, 0),
-                    end: new Date(y, m, d + 1, 22, 30),
-                    allDay: false,
-                }
-            ],
-        });
-
 
     });
+
+
+    /* initialize the calendar
+    -----------------------------------------------------------------*/
+
+    let calendar = $('#calendar').fullCalendar({
+        header: {
+            left: 'title',
+            center: 'agendaDay,agendaWeek,month',
+            right: 'prev,next today'
+        },
+        editable: true,
+        firstDay: 1, //  1(Monday) this can be changed to 0(Sunday) for the USA system
+        selectable: true,
+        defaultView: 'month',
+
+        axisFormat: 'h:mm',
+        columnFormat: {
+            month: 'ddd',    // Mon
+            week: 'ddd d', // Mon 7
+            day: 'dddd M/d',  // Monday 9/7
+            agendaDay: 'dddd d'
+        },
+        titleFormat: {
+            month: 'MMMM yyyy', // September 2009
+            week: "MMMM yyyy", // September 2009
+            day: 'MMMM yyyy'                  // Tuesday, Sep 8, 2009
+        },
+        allDaySlot: false,
+        selectHelper: true,
+
+        select: function (start, end, allDay) {
+            let headline = prompt('headline Title:');
+            let content = prompt('content:');
+            if (headline && content) {
+
+
+                let url = 'https://shotokankarate.herokuapp.com/api/calender';
+                let data = {headline: headline, date: new Date(y, m, d), content: content};
+
+                fetch(url, {
+                    method: "POST",
+                    body: JSON.stringify(data),
+                    headers: {'Content-Type': 'application/json'}
+                })
+                    .then(resultat => {
+                        if (resultat.status >= 400)
+                            throw new Error(resultat.status);
+                        else
+                            return resultat.json();
+                    })
+                    .then(resultat => console.log(`Resultat: %o`, resultat))
+                    .catch(fejl => console.log('Fejl: ' + fejl));
+
+
+                calendar.fullCalendar('renderEvent',
+                    {
+                        headline: headline,
+                        date: date,
+                        content: content,
+                    },
+                    true // make the event "stick"
+                );
+            }
+            calendar.fullCalendar('unselect');
+        },
+        
+
+        events: [
+            {
+                headline: 'Special event i Herning ',
+                content: 'Hele dagen er der baks og riv',
+                date: new Date(y, m, 1),
+
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: new Date(y, m, d - 3, 16, 0),
+                allDay: false,
+                className: 'info'
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: new Date(y, m, d + 4, 16, 0),
+                allDay: false,
+                className: 'info'
+            },
+            {
+                title: 'Meeting',
+                start: new Date(y, m, d, 10, 30),
+                allDay: false,
+                className: 'important'
+            },
+            {
+                title: 'Lunch',
+                start: new Date(y, m, d, 12, 0),
+                end: new Date(y, m, d, 14, 0),
+                allDay: false,
+                className: 'important'
+            },
+            {
+                title: 'Birthday Party',
+                start: new Date(y, m, d + 1, 19, 0),
+                end: new Date(y, m, d + 1, 22, 30),
+                allDay: false,
+            }
+        ],
+    });
+
+
+});
 
