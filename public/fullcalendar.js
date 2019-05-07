fcaf10083ea9b77992f57ca3ca3974bd8ba6b308
JacobVB <jaco7717@gmail.com>
2019-05-07 11:35:10 +0200

    remove async

--- public/fullcalendar.js
+++ public/fullcalendar.js
@@ -483,7 +483,7 @@ function Calendar(element, options, eventSources) {
 	}
 
 
-	async function rerenderEvents(modifiedEventID) { // can be called as an API method
+	function rerenderEvents(modifiedEventID) { // can be called as an API method
 		clearEvents();
 		renderEvents(modifiedEventID);
 	}
