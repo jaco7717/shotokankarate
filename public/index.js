5e22d64cd6325d4f449cea5ed7f5aa857e3c1445
JacobVB <jaco7717@gmail.com>
2019-05-07 11:55:57 +0200

    test

--- public/index.js
+++ public/index.js
@@ -149,7 +149,6 @@ async function toEditMember(id, name, age, email, username, password) {
 
     let data = {name: navn, age: alder, email: mail, username: brugernavn, password: kodeord};
 
-    console.log("per123");
     let url = 'https://shotokankarate.herokuapp.com/api/members/'+id;
 
     fetch(url, {
@@ -168,77 +167,7 @@ async function toEditMember(id, name, age, email, username, password) {
         .catch(fejl => console.log('Fejl: ' + fejl));
 }
 
-$(document).ready(function () {
-    console.log("per")
 
-    /* initialize the calendar
-    -----------------------------------------------------------------*/
-
-    let calendar = $('#calendar').fullCalendar({
-        header: {
-            left: 'title',
-            center: 'month',
-            right: 'prev,next today'
-        },
-        editable: true,
-        firstDay: 1, //  1(Monday) this can be changed to 0(Sunday) for the USA system
-        selectable: true,
-        select: 'month',
-        defaultSelected:  'month',
-        defaultView: 'month',
-
-        axisFormat: 'h:mm',
-        columnFormat: {
-            month: 'ddd',    // Mon
-            week: 'ddd d', // Mon 7
-            day: 'dddd M/d',  // Monday 9/7
-            agendaDay: 'dddd d'
-        },
-        titleFormat: {
-            month: 'MMMM yyyy', // September 2009
-            week: "MMMM yyyy", // September 2009
-            day: 'MMMM yyyy',                  // Tuesday, Sep 8, 2009
-
-        },
-        allDaySlot: false,
-        selectHelper: true,
-
-
-
-        events: {
-            url: 'https://shotokankarate.herokuapp.com/api/calender',
-            type: 'get',
-
-            data: {
-                title: String,
-                date: Date,
-                content: String,
-                className: String,
-                allDay: Boolean,
-            },
-
-            error: function () {
-
-                alert('There was an error while fetching events!');
-            }
-        },
-
-        eventClick: function(info) {
-
-            let tekst = info.content;
-
-            alert(tekst );
-
-
-        },
-
-
-
-
-
-    });
-
-});
 
 
 
--- public/login.js
+++ public/login.js
@@ -35,3 +35,75 @@ async function getNewsUserPage() {
     const compiledTemplate = Handlebars.compile(templateText);
     document.querySelector('#nyhederBrugerside').innerHTML = compiledTemplate({newsUserPage});
 }
+
+$(document).ready(function () {
+    console.log("per")
+
+    /* initialize the calendar
+    -----------------------------------------------------------------*/
+
+    let calendar = $('#calendar').fullCalendar({
+        header: {
+            left: 'title',
+            center: 'month',
+            right: 'prev,next today'
+        },
+        editable: true,
+        firstDay: 1, //  1(Monday) this can be changed to 0(Sunday) for the USA system
+        selectable: true,
+        select: 'month',
+        defaultSelected:  'month',
+        defaultView: 'month',
+
+        axisFormat: 'h:mm',
+        columnFormat: {
+            month: 'ddd',    // Mon
+            week: 'ddd d', // Mon 7
+            day: 'dddd M/d',  // Monday 9/7
+            agendaDay: 'dddd d'
+        },
+        titleFormat: {
+            month: 'MMMM yyyy', // September 2009
+            week: "MMMM yyyy", // September 2009
+            day: 'MMMM yyyy',                  // Tuesday, Sep 8, 2009
+
+        },
+        allDaySlot: false,
+        selectHelper: true,
+
+
+
+        events: {
+            url: 'https://shotokankarate.herokuapp.com/api/calender',
+            type: 'get',
+
+            data: {
+                title: String,
+                date: Date,
+                content: String,
+                className: String,
+                allDay: Boolean,
+            },
+
+            error: function () {
+
+                alert('There was an error while fetching events!');
+            }
+        },
+
+        eventClick: function(info) {
+
+            let tekst = info.content;
+
+            alert(tekst );
+
+
+        },
+
+
+
+
+
+    });
+
+});
