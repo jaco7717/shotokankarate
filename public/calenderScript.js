39bc2cb63b390d1dad9cf288e578ecd6a3029be1
JacobVB <jaco7717@gmail.com>
2019-05-07 08:47:13 +0200

    test

--- public/calenderScript.js
+++ public/calenderScript.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
     /* initialize the external events
     -----------------------------------------------------------------*/
 
-    
+
 
 
     /* initialize the calendar
