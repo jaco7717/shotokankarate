852a38539bb216d895ddb7a50ee34dbfec37ab2c
Henrik de Laurent Andersen <henrikdelaurent@outlook.dk>
2019-05-09 21:07:05 +0200

    stavefejl

--- routes/CalenderRoute.js
+++ routes/CalenderRoute.js
@@ -9,7 +9,7 @@ router
 // GET Calender
 
     .get('/', async (request, response) => {
-        response.json(await calendarModel.find().exec())
+        response.json(await calenderModel.find().exec())
     })
 
     // POST Calender
