288aeb453a201146ef8c4d7048bd96bb2e344546
Henrik de Laurent Andersen <henrikdelaurent@outlook.dk>
2019-05-09 11:36:02 +0200

    . i controlleren

--- controller/controller.js
+++ controller/controller.js
@@ -1,7 +1,7 @@
 "use strict"
 
-const newsModel = require('./models/News')
-const calenderModel = require('./models/Calender')
-const loginModel = require('./models/Login')
-const memberModel = require('./models/Member')
+const newsModel = require('../models/News')
+const calenderModel = require('../models/Calender')
+const loginModel = require('../models/Login')
+const memberModel = require('../models/Member')
 
