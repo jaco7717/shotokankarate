dba3cc4fd04f8f9db4e4741d50fde8c3de8509ad
JacobVB <jaco7717@gmail.com>
2019-05-10 09:12:41 +0200

    dataMedlem

--- public/login.js
+++ public/login.js
@@ -20,9 +20,10 @@ async function loginButton() {
         if (svar.ok)
             window.location.href = "https://shotokankarate.herokuapp.com/session";
         else {
+            const dataMedlem = {email: username.value, password: password.value};
             const resultatMedlem = await fetch("https://shotokankarate.herokuapp.com/api/member", {
                 method: "POST",
-                body: JSON.stringify(data),
+                body: JSON.stringify(dataMedlem),
                 headers: {'Content-Type': 'application/json'}
             });
             const svarMedlem = await resultatMedlem.json();
