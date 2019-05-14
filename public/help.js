0870f675db4d2c59168be3df272e2d912c84f527
Henrik de Laurent Andersen <henrikdelaurent@outlook.dk>
2019-05-14 15:16:33 +0200

    fuck omvendt nyheder

--- public/help.js
+++
@@ -1,7 +0,0 @@
-var helpers = require('diy-handlebars-helpers');
-helpers();
-
-var hbs = require('handlebars-runtime');
-var _   = require('lodash');
-
-_.extend(hbs.helpers, require('diy-handlebars-helpers')());
