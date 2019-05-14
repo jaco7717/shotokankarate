4c77e2c8fc137825d7f249cb127a3a9648fe6884
Henrik de Laurent Andersen <henrikdelaurent@outlook.dk>
2019-05-14 15:03:17 +0200

    sorry

--- public/login.js
+++ public/login.js
@@ -1,10 +1,3 @@
-var helpers = require('diy-handlebars-helpers');
-helpers();
-var hbs = require('handlebars-runtime');
-var _= require('lodash');
-
-_.extend(hbs.helpers, require('diy-handlebars-helpers')());
-
 onload = () => {
     getNewsUserPage();
     loginButton();
