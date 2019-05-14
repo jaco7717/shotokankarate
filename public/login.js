376d1461b712c89eaee646fcbbd737c4bd20c255
JacobVB <jaco7717@gmail.com>
2019-05-14 09:24:19 +0200

    test

--- public/login.js
+++ public/login.js
@@ -1,48 +1,10 @@
 onload = () => {
     getNewsUserPage();
-    loginButton();
+
 
 };
 
-async function loginButton() {
-    const username = document.querySelector('#username');
-    const password = document.querySelector('#password');
-    const button = document.querySelector('#button');
-    const fejl = document.querySelector('#fejl');
 
-    button.onclick = async () => {
-        const data = {username: username.value, password: password.value};
-        const resultat = await fetch("https://shotokankarate.herokuapp.com/login", {
-            method: "POST",
-            body: JSON.stringify(data),
-            headers: {'Content-Type': 'application/json'}
-        });
-        const svar = await resultat.json();
-        if (svar.ok) {
-            window.location.href = "https://shotokankarate.herokuapp.com/session";
-    } else {
-            const dataMedlem = {email: username.value, password: password.value};
-            const resultatMedlem = await fetch("https://shotokankarate.herokuapp.com/member", {
-
-                method: "POST",
-                body: JSON.stringify(dataMedlem),
-                headers: {'Content-Type': 'application/json'}
-            });
-            const svarMedlem = await resultatMedlem.json();
-            if (svarMedlem.ok)
-                window.location.href = "https://shotokankarate.herokuapp.com/memberSession";
-
-            else {
-                fejl.innerHTML = "Login fejl!";
-            }
-        }
-
-        }
-
-
-
-
-}
 
 
 async function getNewsUserPage() {
